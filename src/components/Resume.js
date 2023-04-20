9ed7fec2428f3c19e4830ddaba13c3c2cf615619
stanleyhoDEV <stanleyhoDEV@gmail.com>
2023-04-20 12:33:46 -0400

    started Resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,5 +1,17 @@
 const Resume = () => {
-  return <h1>Resume</h1>;
+  return (
+    <section>
+      <div>
+        <h2>Education</h2>
+      </div>
+      <div>
+        <h2>Experience</h2>
+      </div>
+      <div>
+        <h2>Skills</h2>
+      </div>
+    </section>
+  );
 };
 
 export default Resume;
