5640c22bdf5d95bc2cc9d9a45ea7b27e07458622
stanleyhoDEV <stanleyhoDEV@gmail.com>
2023-05-05 12:33:20 -0400

    updated Projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 const Projects = () => {
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState(true);
 
   console.log(show);
 
@@ -9,14 +9,7 @@ const Projects = () => {
     <section className="projects">
       <h2>Projects</h2>
       <div>
-        <button
-          onClick={() => {
-            setShow(!show);
-          }}
-        >
-          Project 1 name
-        </button>
-        <a href="https://www.stanleyho.org">
+        <a href="https://maymarpoke.com/" target="_blank" rel="noreferrer">
           <img
             src="https://www.liquidplanner.com/wp-content/uploads/2019/04/HiRes-17.jpg"
             alt="project"
@@ -27,17 +20,43 @@ const Projects = () => {
             setShow(!show);
           }}
         >
-          show/hide
+          <h3>&#91; Project 1 &#93;</h3>
         </button>
         {show && (
           <p>
-            DESCRIPTION: Excepteur non adipisicing laboris officia ipsum
-            reprehenderit magna. Ex amet in proident minim duis cillum sit id
-            mollit veniam. Cillum officia tempor officia do. Dolore enim aute
-            anim sint elit occaecat consequat tempor dolor in laborum qui velit.
-            Minim dolore adipisicing elit ea sint ut. Irure cillum ex laboris
-            commodo labore cupidatat excepteur incididunt duis tempor nisi ea
-            elit. Amet pariatur sunt deserunt excepteur.
+            Excepteur non adipisicing laboris officia ipsum reprehenderit magna.
+            Ex amet in proident minim duis cillum sit id mollit veniam. Cillum
+            officia tempor officia do. Dolore enim aute anim sint elit occaecat
+            consequat tempor dolor in laborum qui velit. Minim dolore
+            adipisicing elit ea sint ut. Irure cillum ex laboris commodo labore
+            cupidatat excepteur incididunt duis tempor nisi ea elit. Amet
+            pariatur sunt deserunt excepteur.
+          </p>
+        )}
+      </div>
+      <div>
+        <a href="https://maymarpoke.com/" target="_blank" rel="noreferrer">
+          <img
+            src="https://www.liquidplanner.com/wp-content/uploads/2019/04/HiRes-17.jpg"
+            alt="project"
+          />
+        </a>
+        <button
+          onClick={() => {
+            setShow(!show);
+          }}
+        >
+          <h3>&#91; Project 2 &#93;</h3>
+        </button>
+        {show && (
+          <p>
+            Excepteur non adipisicing laboris officia ipsum reprehenderit magna.
+            Ex amet in proident minim duis cillum sit id mollit veniam. Cillum
+            officia tempor officia do. Dolore enim aute anim sint elit occaecat
+            consequat tempor dolor in laborum qui velit. Minim dolore
+            adipisicing elit ea sint ut. Irure cillum ex laboris commodo labore
+            cupidatat excepteur incididunt duis tempor nisi ea elit. Amet
+            pariatur sunt deserunt excepteur.
           </p>
         )}
       </div>
