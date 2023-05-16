34cfb82ba93142d487f26264188283b55843f478
stanleyhoDEV <stanleyhoDEV@gmail.com>
2023-05-16 16:04:43 -0400

    worked on Navigation component for transition from mobile to tablet

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,28 @@
 import { GiHamburgerMenu } from "react-icons/gi";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+// import { useMediaQuery } from "react-responsive";
 
 const Navigation = ({ data }) => {
   const sections = data.navigation;
   const [showNav, setShowNav] = useState(false);
+  const [count, setCount] = useState(0);
+
+  // will be using useEffect to listen for window size adjustments.
+  useEffect(() => {
+    console.log("effect");
+  }, []);
 
   console.log(showNav);
   return (
     <nav>
+      <p style={{ color: "white" }}>{count}</p>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        btn
+      </button>
       <button className="nav-btn" onClick={() => setShowNav(!showNav)}>
         <GiHamburgerMenu className="hamburger-icon" />
       </button>
