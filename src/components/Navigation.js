5643282b8062236ffc09318aabc133fda6f60457
stanleyhoDEV <stanleyhoDEV@gmail.com>
2023-05-19 15:06:33 -0400

    fixed transition from mobile to non-mobile Navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,71 +1,59 @@
 import { GiHamburgerMenu } from "react-icons/gi";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 const Navigation = ({ data }) => {
   const sections = data.navigation;
   // Show or hide navigation.
-  const [showNav, setShowNav] = useState(true);
-
-  // Show or hide mobile navigation based on screen size.
-  const [showMobileNav, setShowMobileNav] = useState(false);
-  const [size, setSize] = useState(window.innerWidth);
-
-  // Checks the size of the window to show or hide the mobile navigation menu.
-  const checkSize = () => {
-    if (size < 767) {
-      setShowNav(false);
-      setShowMobileNav(true);
-    } else if (size > 767) {
-      setShowNav(true);
-      setShowMobileNav(false);
-    }
-    setSize(window.innerWidth);
-  };
-
-  // useEffect to add an event listener for window size adjustments.
-  useEffect(() => {
-    window.addEventListener("resize", checkSize);
-
-    // cleanup Listener
-    return () => {
-      window.removeEventListener("resize", checkSize);
-    };
-    // eslint-disable-next-line
-  }, [size]);
+  const [showNav, setShowNav] = useState(false);
 
   return (
     <nav>
-      {showMobileNav && (
+      <div id="hide-mobile" className="mobile-nav">
         <button className="nav-btn" onClick={() => setShowNav(!showNav)}>
           <GiHamburgerMenu className="hamburger-icon" />
         </button>
-      )}
 
-      <div className={showNav ? "nav-list-active" : "nav-list"}>
+        <div className={showNav ? "nav-list-active" : "nav-list"}>
+          {showNav && (
+            <ul>
+              {sections.map((section, index) => {
+                return (
+                  <li key={index}>
+                    <a
+                      href={`#${section}`}
+                      onClick={() => setShowNav(!showNav)}
+                    >
+                      {section}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          )}
+        </div>
+        {/* A large transparent button after the navigation bar to close the navigation bar.*/}
         {showNav && (
-          <ul>
-            {sections.map((section, index) => {
-              return (
-                <li key={index}>
-                  <a href={`#${section}`} onClick={() => setShowNav(!showNav)}>
-                    {section}
-                  </a>
-                </li>
-              );
-            })}
-          </ul>
+          <div className="close-nav">
+            <button
+              className="close-nav-btn"
+              onClick={() => setShowNav(!showNav)}
+            ></button>
+          </div>
         )}
       </div>
-
-      {/* A large transparent button after the navigation bar to close the navigation bar.*/}
-      {showNav && (
-        <div className="close-nav">
-          <button
-            className="close-nav-btn"
-            onClick={() => setShowNav(!showNav)}
-          ></button>
-        </div>
-      )}
+      <div id="hide-non-mobile" className="non-mobile-nav">
+        <ul>
+          {sections.map((section, index) => {
+            return (
+              <li key={index}>
+                <a href={`#${section}`} onClick={() => setShowNav(!showNav)}>
+                  {section}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </nav>
   );
 };
