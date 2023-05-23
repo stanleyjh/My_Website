7c573c8687a63c372e5f3adc98737b52384c18a0
stanleyhoDEV <stanleyhoDEV@gmail.com>
2023-05-23 15:01:45 -0400

    added break lines

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 const Contact = () => {
   return (
-    <section className="contact">
+    <section id="contact" className="contact">
       <h2>Get in touch!</h2>
       <div className="contact--mobile">
         <p>stanleyho619@gmail.com</p>
@@ -8,17 +8,24 @@ const Contact = () => {
         <p>linkedin</p>
         <form>
           <label for="name"></label>
-          <input type="text" id="name" name="name" placeholder="Name" />
+          <input type="text" id="name" name="name" placeholder="Name" /> <br />
           <label for="email"></label>
-          <input type="email" id="email" name="email" placeholder="Email" />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+          />{" "}
+          <br />
           <label for="message"></label>
           <input
             type="text"
             id="message"
             name="message"
             placeholder="Message"
-          />
-          <input type="submit" />
+          />{" "}
+          <br />
+          <input type="submit" value="Send" /> <br />
           <input type="reset" value="Clear"></input>
         </form>
       </div>
